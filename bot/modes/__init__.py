8c1a17204732ae304ead4df1135a7253080e9f90
winstxnhdw <winstxnhdw@gmail.com>
2022-07-25 21:04:48 +0800

    update

--- bot/modes/__init__.py
+++ bot/modes/__init__.py
@@ -3,5 +3,5 @@ from bot.modes.grind import Grind
 from bot.modes.exp_grind import ExperienceGrind
 from bot.modes.gold_grind import GoldGrind
 from bot.modes.charge_kindred import ChargeKindred
-from bot.modes.charge_flame_tome import ChargeBookOfBurns
+from bot.modes.charge_book_of_burns import ChargeBookOfBurns
 from bot.modes.skip_dialogue import SkipDialogue
